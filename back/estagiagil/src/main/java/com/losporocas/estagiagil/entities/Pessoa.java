7aa87b0207858fc1cfb4fcfd7cbfe1c26f668cd1
Wesllen Santos <wesllen.2020111052@unicap.br>
2022-06-01 16:28:53 -0300

    backend do springboot inserido e com entities

---
+++ back/estagiagil/src/main/java/com/losporocas/estagiagil/entities/Pessoa.java
@@ -0,0 +1,47 @@
+package com.losporocas.estagiagil.entities;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class Pessoa {
+	
+	@Id
+	private int id;
+	private String nome;
+	private String email;
+
+	public Pessoa() {
+		
+	}
+	
+	public Pessoa(int id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
---
+++ back/estagiagil/src/test/java/com/losporocas/estagiagil/EstagiagilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losporocas.estagiagil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstagiagilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
