683c6d7c1203623471e27755ec6e08508bd7cbbf
Wesllen Santos <wesllen.2020111052@unicap.br>
2022-06-02 21:22:08 -0300

    testes de SQL + alterações nos atributos das classes

--- back/estagiagil/src/main/resources/import.sql
+++ back/estagiagil/src/main/resources/import.sql
@@ -1,6 +1,8 @@
 INSERT INTO coordenadores(id, nome, email, area) VALUES (2017202502, 'Frodo', 'anel@gmail.com', 'hobbit');
 INSERT INTO coordenadores(id, nome, email, area) VALUES (2017202501, 'Sam', 'lembas@gmail.com', 'hobbit');
 
-
-
 INSERT INTO alunos(id, nome, email, estagiando, periodo, status_aluno) VALUES (2020111052, 'Wes', 'wessan@gmail.com', false, 5, 'Buscando estágio');
+INSERT INTO alunos(id, nome, email, estagiando, periodo, status_aluno, coordenador_id) VALUES (2020111051, 'Sew', 'wessew@gmail.com', true, 5, 'Buscando estágio', 2017202502);
+
+INSERT INTO vagas(id, requisito, descricao) VALUES (1, 'OO, C#, .NET,Scrum','ESTÁGIO EM DESENVOLVIMENTO DE SOFTWARE - AVANADE');
+INSERT INTO vagas(id, requisito, descricao) VALUES (2, 'Scrum, Kanban, SQL Server, Pacote Office, Quinto período ou superior.','ESTÁGIO EM GESTÃO DE PROJETOS - AVANADE');
